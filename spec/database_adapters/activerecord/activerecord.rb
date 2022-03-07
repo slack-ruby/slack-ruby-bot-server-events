66af688f998d65a93d47d353e336816b6b32706b
crazyoptimist <crazyoptimist@mail.com>
2022-03-07 13:08:15 -0600

    fix typo

--- spec/database_adapters/activerecord/activerecord.rb
+++ spec/database_adapters/activerecord/activerecord.rb
@@ -2,7 +2,7 @@
 
 db_config = YAML.safe_load(
   ERB.new(File.read(
-            File.expand_path('config/postgresql.yml', __dir__)
+            File.expand_path('postgresql.yml', __dir__)
           )).result, [], [], true
 )[ENV['RACK_ENV']]
 ActiveRecord::Tasks::DatabaseTasks.create(db_config)
